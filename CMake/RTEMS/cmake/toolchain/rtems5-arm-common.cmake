1dd301bb5db2c1ca1e1c08c732c7a871306828f3
kaidoho <kho237115@gmail.com>
2020-03-10 21:07:51 +0100

    Update cmake files

--- CMake/RTEMS/cmake/toolchain/rtems5-arm-common.cmake
+++ CMake/RTEMS/cmake/toolchain/rtems5-arm-common.cmake
@@ -5,24 +5,9 @@ set(RTEMS_TOP_ARCH  arm)
 
 
 
-list(APPEND RTEMS_CPU_FLAGS -mthumb)
-list(APPEND RTEMS_CPU_FLAGS -mtune=${RTEMS_TARGET_CPU} )
+list(APPEND RTEMS_CPU_FLAGS -mthumb -mthumb-interwork)
+list(APPEND RTEMS_CPU_FLAGS -march=armv7e-m+fp.dp)
 
-if(DEFINED RTEMS_FPU_ABI)
-    list(APPEND RTEMS_CPU_FLAGS -mfloat-abi=${RTEMS_FPU_ABI})
-endif()
-
-if(DEFINED RTEMS_FPU_TYPE)
-    list(APPEND RTEMS_CPU_FLAGS -mfpu=${RTEMS_FPU_TYPE})
-endif()
-
-
-
-if( "${RTEMS_TARGET_CPU}" MATCHES "cortex-m7")
-    list(APPEND RTEMS_CPU_FLAGS -march=armv7e-m)
-else()
-    message(FATAL_ERROR "Unknown CPU ${RTEMS_TARGET_CPU}")
-endif()
 
 
 include(${CMAKE_CURRENT_LIST_DIR}/rtems5-common.cmake)
